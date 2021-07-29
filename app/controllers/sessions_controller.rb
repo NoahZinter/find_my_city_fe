da1da98b4e60b7514ddcb5abb9629c9e8c907e61
JoePeterson51 <>
2021-07-28 19:46:27 -0600

    Impliment google oauth

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,26 @@
+class SessionsController < ApplicationController
+
+  def new
+
+  end
+
+  def create
+    user = User.find_or_create_by(email: user_attributes[:email])
+    user.update(user_attributes)
+    session[:user_id] = user.id
+    redirect_to '/dashboard'
+  end
+
+  private
+  def auth_hash
+    request.env['omniauth.auth']
+  end
+
+  def user_attributes
+    {
+      google_id: auth_hash['uid'],
+      email: auth_hash['info']['email'],
+      token: auth_hash['credentials']['token']
+    }
+  end
+end
