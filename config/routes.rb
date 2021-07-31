1d99bc14e3cb0b2710ed76064accf374030cf8b9
NoahZinter <noah.zinter@gmail.com>
2021-07-31 12:19:27 -0600

    Incorporate state select helper

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   get '/', to: 'welcome#index', as: 'root'
 
+  get '/search', to: 'search#index'
+
   get '/auth/:provider/callback', to: 'sessions#create'
+
   get '/dashboard', to: 'dashboard#show'
 
   resources :cities, only: :show
