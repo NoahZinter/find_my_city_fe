a2112f5981d04b75052dcdbcf80e44aaaaa7ff54
NoahZinter <noah.zinter@gmail.com>
2021-08-01 12:13:20 -0600

    Test CityService

--- app/poros/city_detail.rb
+++ app/poros/city_detail.rb
@@ -1,4 +1,4 @@
-class City
+class CityDetail
   attr_reader :image,
               :name,
               :total_score,
--- app/services/city_service.rb
+++ app/services/city_service.rb
@@ -1,5 +1,5 @@
-class CityDetailsService
-  def self.get_categories(city, state)
+class CityService
+  def self.get_city(city, state)
     url = 'https://helpmefindmycity-api.herokuapp.com/api/v1/city'
     response = Faraday.get(url, { city: city, state: state})
     JSON.parse(response.body, symbolize_names: true)
