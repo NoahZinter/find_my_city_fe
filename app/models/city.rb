2511a0621e30141a99327cac0d902158547c6fd0
Joe Ray <jray100.jr@gmail.com>
2021-08-01 12:20:15 -0600

    Update/create User, Favorite, City models

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,2 +1,8 @@
 class City < ApplicationRecord
+  serialize :categories_hash_array, Array
+
+  validates_presence_of :categories_hash_array
+
+  has_many :favorites
+  has_many :users, through: :favorites
 end
