25f32df059b8eb25904857fdbadf626e1a224c8d
NoahZinter <noah.zinter@gmail.com>
2021-08-01 18:23:29 -0600

    Fix service spacing

--- app/services/city_service.rb
+++ app/services/city_service.rb
@@ -1,5 +1,6 @@
 class CityService
   def self.get_city(city, state)
+    city = ERB::Util.url_encode(city)
     url = 'https://helpmefindmycity-api.herokuapp.com/api/v1/city'
     response = Faraday.get(url, { city: city, state: state})
     JSON.parse(response.body, symbolize_names: true)
