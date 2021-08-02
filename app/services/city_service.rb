696171b83bb91913c3b4c303f34541d77d131008
NoahZinter <noah.zinter@gmail.com>
2021-08-02 11:17:07 -0600

    Gsub empty spaces in city service

--- app/services/city_service.rb
+++ app/services/city_service.rb
@@ -1,5 +1,6 @@
 class CityService
   def self.get_city(city, state)
+    city = city.gsub(/[' ']/, '%20')
     url = 'https://helpmefindmycity-api.herokuapp.com/api/v1/city'
     response = Faraday.get(url, { city: city, state: state})
     JSON.parse(response.body, symbolize_names: true)
