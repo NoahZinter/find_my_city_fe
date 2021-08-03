91a830a44e388c78769f5243eea4bdf62531d18f
ahenzler <59706432+ahenzler@users.noreply.github.com>
2021-08-02 18:26:31 -0600

    weather controller

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,6 @@
 class SearchController < ApplicationController
   def show
     @city = CityFacade.show_city(params[:city], params[:state])
+    @city_weather = WeatherFacade.weather(params[:city], params[:state])
   end
 end
