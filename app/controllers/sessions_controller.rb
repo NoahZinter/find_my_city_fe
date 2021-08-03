682de059f8760acbe85dd850960e2ee5e2172c00
JoePeterson51 <>
2021-08-03 16:54:08 -0600

    Add Login without oauth

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,21 @@
 class SessionsController < ApplicationController
 
   def create
-    user = User.find_or_create_by(email: user_attributes[:email])
-    user.update(user_attributes)
-    session[:user_id] = user.id
-    redirect_to '/'
+    if params["password"].present?
+      params["email"] = params["email"].downcase
+      if User.where('email = ?', "#{params[:email]}").nil?
+        user = User.create!(user_params)
+        session[:user_id] = user.id
+      else
+        user = User.where('email = ?', "#{params[:email]}")
+        session[:user_id] = user[0].id
+      end
+    else params["token"].present?
+      user = User.find_or_create_by(email: user_attributes[:email])
+      user.update(user_attributes)
+      session[:user_id] = user.id
+    end
+    redirect_to '/dashboard'
   end
 
   def destroy
@@ -23,5 +34,9 @@ class SessionsController < ApplicationController
       email: auth_hash['info']['email'],
       token: auth_hash['credentials']['token']
     }
+  end
+
+  def user_params
+    params.permit(:email, :password)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,10 @@
 class User < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_many :cities, through: :favorites
+
+  validates :email, uniqueness: true, presence: true
+  validates_presence_of :password_digest, require: true
+  validates_confirmation_of :password_digest
+
+  has_secure_password
 end
