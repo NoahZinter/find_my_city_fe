ce3a6cba7fb622d3d3ca2e7c229a2a22c82d25f3
Joe Ray <jray100.jr@gmail.com>
2021-08-04 20:41:06 -0600

    Add new endpoint method for users favorites list

--- app/services/favorites_service.rb
+++ app/services/favorites_service.rb
@@ -1,8 +1,13 @@
 class FavoritesService
   class << self
-    def user_favorites_search(user_id, city_id)
+    def user_adds_favorites(user_id, city_id)
       url = 'https://helpmefindmycity-api.herokuapp.com/api/v1/favorites'
       response = Faraday.post(url, { user_id: user_id, city_id: city_id } )
+      JSON.parse(response.body, symbolize_names: true)
+    end
+
+    def user_favorites_list(user_id)
+      response = Faraday.get("https://helpmefindmycity-api.herokuapp.com/api/v1/users/#{user_id}/favorites")
       JSON.parse(response.body, symbolize_names: true)
     end
   end
