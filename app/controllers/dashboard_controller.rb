018fee91e4d01e0567dba993c81fc9c6afa44bbe
Joe Ray <jray100.jr@gmail.com>
2021-08-04 18:30:29 -0600

    Fix all errors for FE app

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,4 +1,7 @@
 class DashboardController < ApplicationController
+  def index
+  end
+
   def show
     @user = current_user
     @city = City.find(params[:city_id])
--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,5 @@ Rails.application.routes.draw do
 
   delete '/logout', to: 'sessions#destroy'
 
-  resources :dashboard, only: :show
+  resources :dashboard, only: [:index, :show]
 end
